b6b937d9513cdf1ad440ef371131891b940630e4
StanleyGuo <Stanley1234@users.noreply.github.com>
2018-06-14 11:42:30 -0400

    still brutal, rearrange codes 190ms -> 9ms

--- LeetCode/486 - Predict the Winner - version1.java
+++ LeetCode/486 - Predict the Winner - version1.java
@@ -6,23 +6,15 @@ class Solution {
             if (p1 >= p2) { return true; }
             else { return false; }
         }
-        boolean res1, res2;
-        if (player) {
-            // player 1 
-            res1 = helper(nums, left + 1, right, p1 + nums[left], p2, !player);
-            res2 = helper(nums, left, right - 1, p1 + nums[right], p2, !player);
-        } else {
-            // player 2
-            res1 = helper(nums, left + 1, right, p1, p2 + nums[left], !player);
-            res2 = helper(nums, left, right - 1, p1, p2 + nums[right], !player);
-        }
         if (player) {
             // for player 1, he wants to choose the strategy that he will definitely win at this step
-            return res1 || res2;
+            return helper(nums, left + 1, right, p1 + nums[left], p2, !player) 
+                || helper(nums, left, right - 1, p1 + nums[right], p2, !player);
         } else {
             // for player 2, if one strategy allows him to win, then player 1 loses
             // if there is a 'false', then player 1 loses from this step
-            return res1 && res2;
+            return helper(nums, left + 1, right, p1, p2 + nums[left], !player)
+                && helper(nums, left, right - 1, p1, p2 + nums[right], !player);
         }
     }
     
